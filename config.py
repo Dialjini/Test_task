8e2882f8578592d54c6cf885b75c45fe71be0aad
Dialjini <danil_kustov@mail.ru>
2021-05-02 15:08:19 +0700

    remove Flask and set aiohttp up

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-DEBUG = True
-SECRET_KEY = 'f932b03c797e880432f11149'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
